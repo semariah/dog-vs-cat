eaf5beace6750171e16e81433aa30ad913f708f6
semhar <semaria.eyzac@gmal.com>
2018-02-09 12:27:39 -0800

    Add new js features

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,36 @@
 $(document).ready(function(){
   $(".clickable").click(function(){
-    $(#show-photo).toggle();
-    $(#hide-photo).toggle();
+    $("#funphoto-show").toggle();
+    $("#funphoto-hide").toggle();
   });
 });
+
 $(document).ready(function(){
   $(".press").click(function(){
-    $(#show).toggle();
-    $(#hide).toggle();
+    $("#photo-show").toggle();
+    $("#photo-hide").toggle();
   });
+});
+
+$(document).ready(function(){
+  $("button#bark").click(function(){
+    $("ul#Dog").prepend("<li>hei catty, Does human hair dye works on dogs?</li>");
+    $("ul#Cat").prepend("<li>No</li>");
+    $("ul#Dog").children("li").first().click(function(){
+      $(this).remove();
+    });
+    $("ul#Cat").children("li").first().click(function(){
+      $(this).remove();
+    });
+  });
+  $("button#meow").click(function(){
+    $("ul#Dog").prepend("<li>did you say that to just me up?</li>");
+    $("ul#Cat").prepend("<li>yes</li>");
+    $("ul#Dog").children("li").first().click(function(){
+      $(this).remove();
+    });
+    $("ul#Cat").children("li").first().click(function(){
+      $(this).remove();
+    });
+  })
 });
