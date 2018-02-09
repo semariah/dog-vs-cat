56853abbf0bf2b68d9b6742d65e57eaaf0ba8149
semhar <semaria.eyzac@gmal.com>
2018-02-09 13:41:45 -0800

    modify with more js features

--- js/scripts.js
+++ js/scripts.js
@@ -5,82 +5,81 @@ $(document).ready(function(){
   });
 
 
-$(document).ready(function(){
+
   $(".press").click(function(){
     $("#photo-show").toggle();
     $("#photo-hide").toggle();
   });
-});
 
-$(document).ready(function(){
+
+
   $("button#bark").click(function(){
     $("ul#Dog").append("<li>Bark, Bark, Bark catty, Does human hair dye works on dogs?</li>");
     $("ul#Dog").children("li").first().click(function(){
       $(this).remove();
     });
   });
-});
 
-$(document).ready(function(){
+
+
   $("button#meow").click(function(){
     $("ul#Cat").append("<li>meowwww hell no silly!</li>");
     $("ul#Cat").children("li").first().click(function(){
       $(this).remove();
     });
   });
-});
 
-$(document).ready(function(){
+
+
   $("button#bark").click(function(){
     $("ul#Dog").append("<li>Bark, Bark, Bark did you just say that to shut me up?</li>");
     $("ul#Dog").children("li").first().click(function(){
       $(this).remove();
     });
   });
-});
 
-$(document).ready(function(){
+
   $("button#meow").click(function(){
     $("ul#Cat").append("<li>meowww yes!</li>");
     $("ul#Cat").children("li").first().click(function(){
       $(this).remove();
     });
   });
-});
 
-$(document).ready(function(){
+
+
   $("button#bark").click(function(){
     $("ul#Dog").append("<li>Bark, Bark, Don't you want to know why i am asking?</li>");
     $("ul#Dog").children("li").first().click(function(){
       $(this).remove();
     });
   });
-});
 
-$(document).ready(function(){
+
+
   $("button#meow").click(function(){
     $("ul#Cat").append("<li>meowww you want to dye yourself blonde?!</li>");
     $("ul#Cat").children("li").first().click(function(){
       $(this).remove();
     });
   });
-});
 
-$(document).ready(function(){
+
+
   $("button#bark").click(function(){
     $("ul#Dog").append("<li>Bark, Yes!! I'd look like madonna!</li>");
     $("ul#Dog").children("li").first().click(function(){
       $(this).remove();
     });
   });
-});
 
-$(document).ready(function(){
+
+
   $("button#meow").click(function(){
     $("ul#Cat").append("<li>meowww oh stop it!</li>");
     $("ul#Cat").children("li").first().click(function(){
       $(this).remove();
     });
   });
-});
+
 });
